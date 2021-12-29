cb08976ac0aba93126c33b4dafec92a1ebd3375a
NothingIsLost <nothingislost@fastmail.net>
2021-12-28 16:29:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Obsidian Highlight Current Word
+
+An experimental Obsidian plugin that highlights all occurrences of the word under the cursor
+
+### Limitations
+
+Only words with 3 characters or more will be highlighted
+
+Substrings will be matched so putting your cursor on the word "the" will highlight "toge`the`r", "`the`re" etc
+
+### Installing via BRAT
+
+Install the BRAT plugin via the Obsidian Plugin Browser and then add the beta repository "nothingislost/obsidian-..."
+
+### Manually installing the plugin
+
+- Copy over `main.js`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/obsidian-.../`.
---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import { Plugin } from "obsidian";
+
+export default class CM6ExamplesPlugin extends Plugin {
+  async onload() {
+    this.registerEditorExtension(markCurrentWordPlugin());
+  }
+}
+
+import { Decoration, DecorationSet, EditorView, MatchDecorator, ViewPlugin, ViewUpdate } from "@codemirror/view";
+
+function markCurrentWordPlugin() {
+  return ViewPlugin.fromClass(
+    class {
+      decorations: DecorationSet = Decoration.none;
+      decorator: MatchDecorator;
+
+      constructor(public view: EditorView) {
+        this.decorator = this.makeDecorator(view);
+        if (this.decorator) {
+          this.decorations = this.decorator?.createDeco(this.view);
+        } else {
+          this.decorations = Decoration.none;
+        }
+      }
+
+      makeDecorator(view: EditorView) {
+        let word = view.state.wordAt(view.state.selection.main.from);
+        let wordText = word && view.state.doc.sliceString(word.from, word.to);
+        console.log(wordText);
+        if (!wordText || wordText.length < 3) return;
+        return new MatchDecorator({
+          regexp: new RegExp(wordText, "g"),
+          decoration: () => {
+            return Decoration.mark({ class: "cm-current-word" });
+          },
+        });
+      }
+
+      update(update: ViewUpdate) {
+        // this is overkill to try and get the regex to update to 
+        // search for the current word under the cursor at all times
+        this.decorator = this.makeDecorator(this.view);
+        if (this.decorator) {
+          this.decorations = this.decorator?.createDeco(this.view);
+        } else {
+          this.decorations = Decoration.none;
+        }
+      }
+    },
+    {
+      decorations: v => v.decorations,
+    }
+  );
+}
